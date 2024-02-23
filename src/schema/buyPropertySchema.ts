f14ae19c1cccdd55b4ddea30431d3a64dee4c3fb
Rohit <rohitkumar893832@gmail.com>
2024-02-23 18:38:48 +0530

    add the property schema

--- src/schema/buyPropertySchema.ts
+++ src/schema/buyPropertySchema.ts
@@ -1,34 +1,28 @@
 import mongoose, { Document, Model, Schema ,ObjectId} from "mongoose";
 
-// Define an interface for the SellProperty document
-interface ISellProperty extends Document {
+// Define an interface for the BuyProperty document
+interface IBuyProperty extends Document {
     propertyType: string;
     district: string;
     taluka: string;
-    // state: string;
-    locationURL?: string;
-    uploadFilePath: string[];
-    amount: number;
+    audio?: string;
     descriptions: string;
     isDeleted: boolean;
     userId:ObjectId;
 }
 
 // Define the schema
-const sellPropertySchema: Schema<ISellProperty> = new Schema({
+const buyPropertySchema: Schema<IBuyProperty> = new Schema({
     userId: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
     propertyType: { type: String,enum:["residential", "commercial", "land/plot"], default: null },
     taluka: { type: String, default: null },
     district: { type: String, default: null },
-    // state: { type: String, default: null },
-    locationURL: { type: String },
-    uploadFilePath: [{ type: String }],
-    amount: { type: Number, default: null },
+    audio: { type: String },
     descriptions: { type: String, default: null },
     isDeleted : { type: Boolean, default: false },
 });
 
 // Create the model
-const SellProperty: Model<ISellProperty> = mongoose.model<ISellProperty>('SellProperty', sellPropertySchema, 'sellproperty');
+const BuyProperty: Model<IBuyProperty> = mongoose.model<IBuyProperty>('BuyProperty', buyPropertySchema, 'buyproperty');
 
-export default SellProperty;
+export default BuyProperty;
